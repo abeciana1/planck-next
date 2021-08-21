850f5c768841c9c5699454a5ef5b4e6727faad7f
Alex <alex.beciana@gmail.com>
2021-08-20 23:56:55 -0400

    Started creation of wrapping component

---
+++ components/layouts/PagePaper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PagePaper = ({children}) => {
+
+
+    return (
+        <React.Fragment>
+            <section>
+                {children}
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default PagePaper
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      ...defaultTheme.colors,
+      white: "#FBFEF9",
+      black: "#040303",
+      blue: "#283098",
+      yellow: "#FAB92F",
+      red: "#E73508"
+    }
   },
   variants: {
     extend: {},
